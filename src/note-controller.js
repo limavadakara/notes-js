f7f35c75875e41e9f4a5ff4fb1425101e2677d16
Lima Vadakara <lima.vadakara@gmail.com>
2019-03-24 01:35:36 +0000

    Add functionality to show content of single note without refreshing the page

--- src/note-controller.js
+++ src/note-controller.js
@@ -8,13 +8,23 @@
     var noteListViewFunction = NoteListView
     var note = new Note("Favourite drink: seltzer")
     noteList.storeNote(note)
+    listen();
+    function listen(){
+      window.addEventListener("hashchange", function(){
+      var noteHashToDisplay = window.location.hash
+      var noteIdToShow = parseInt(noteHashToDisplay.slice(-1), 10)
+      var noteToShow = noteList.getNotes()[noteIdToShow]
+      document.getElementById("app").innerHTML = "<div>"+noteToShow.getText()+"</div>"
+      })
+    }
     function updateWindow(){
       var noteListView = new NoteListView(noteList)
       document.getElementById("app").innerHTML = noteListView.view()
     }
 
     return {
-      updateWindow: updateWindow
+      updateWindow: updateWindow,
+      listen: listen
     }
   }
   exports.NoteController = NoteController;
--- tests/note-controller-tests.js
+++ tests/note-controller-tests.js
@@ -1,33 +1,60 @@
 (function(exports){
   function testThatAControllerGetsInstantiatedAndItSetsTheRightHTMLStringOnTheRightDOMElement() {
     function NoteDouble(text) {}
-    NoteDouble.prototype = {
-      getText: function(){
-        return "First Text"
-      }
-    }
+
 
     noteListDouble = {
-                        notes: [],
+
                         getNotes: function(){
                                     return this.notes
                                   },
                         storeNote: function(note){
-                          this.notes.push(note)
+
                         }
                     }
     function NoteListViewDouble(noteList) {
                     }
     NoteListViewDouble.prototype = {
       view: function(){
-              return "<ul><li><div>First Text</div></li></ul>"
+              return '<ul><li><div><a href="#notes/0">First Text</a></div></li></ul>'
       }
     }
     controller = new NoteController(noteListDouble, NoteListViewDouble, NoteDouble)
     controller.updateWindow()
-
-    assert.isTrue(document.getElementById("app").innerHTML === "<ul><li><div>First Text</div></li></ul>")
+    assert.isTrue(document.getElementById("app").innerHTML === '<ul><li><div><a href="#notes/0">First Text</a></div></li></ul>')
 
   };
   testThatAControllerGetsInstantiatedAndItSetsTheRightHTMLStringOnTheRightDOMElement();
+
+  function testThatTheSingleNoteViewIsOpenedForTheSelectedNoteWhenItsURLIsClickedOnTheDOM() {
+    function NoteDouble2(text) {}
+    function NoteListViewDouble2(noteList) {}
+    noteListDouble2 = {
+
+                        getNotes: function(){
+                          return [{ id: 0,
+                                    getText: function(){
+                                      return "A long text that is more than 20 characters"
+                                                    }
+                                      }]
+                                  },
+                        storeNote: function(note){
+
+                        }
+                    }
+    controller = new NoteController(noteListDouble2, NoteListViewDouble2, NoteDouble2)
+    async function getResponseFromLinkClick(){
+      let promise = new Promise(function(resolve, reject){
+        document.links[0].click();
+
+        setTimeout(() => resolve(true), 100)
+      });
+      let result = await promise;
+      assert.isTrue(document.getElementById("app").innerHTML === '<div>A long text that is more than 20 characters</div>')
+    }
+
+    getResponseFromLinkClick();
+
+  };
+  testThatTheSingleNoteViewIsOpenedForTheSelectedNoteWhenItsURLIsClickedOnTheDOM();
 })(this);
